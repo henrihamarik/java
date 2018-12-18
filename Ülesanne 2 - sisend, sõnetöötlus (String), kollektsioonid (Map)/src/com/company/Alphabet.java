7206c1e4f4f5b2f7130dbc8ce035f6c9a67ca3a9
henrihamarik <33862312+henrihamarik@users.noreply.github.com>
2018-12-18 11:43:51 +0200

    Add files via upload

---
+++ Ülesanne 2 - sisend, sõnetöötlus (String), kollektsioonid (Map)/src/com/company/Alphabet.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.HashMap;
+public class Alphabet {
+    private static final HashMap<Character, String[]> alphabet = new HashMap<>();
+    static {
+        alphabet.put('a', new String[]{
+                " ###  ",
+                "#   # ",
+                "##### ",
+                "#   # ",
+                "#   # "
+        });
+    }
+    public static HashMap<Character, String[]> getAlphabet() {
+        return alphabet;
+    }
+}
